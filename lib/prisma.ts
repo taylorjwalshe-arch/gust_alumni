30b399d77b4900f20f509bf6c7e6a0ff27bd750c
taylorjwalshe-arch <taylorjwalshe@gmail.com>
2025-09-23 17:55:48 -0400

    fix(ticket-15): require import fallback for PrismaClient in Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+let { PrismaClient } = require("@prisma/client");
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
 }
 
 const prisma =
